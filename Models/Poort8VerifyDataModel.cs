7cf56f7bac0a97df243c1431407c811f861af794
vinhnguyenx1 <Vinhn4@gmail.com>
2022-06-06 15:32:50 +0700

    Create Poort8VerifyDataModel.cs

---
+++ Models/Poort8VerifyDataModel.cs
@@ -0,0 +1,7 @@
+    public class Poort8VerifyDataModel
+    {
+        public string GenericKey { get; set; }
+        public int GenericType { get; set; }
+        public string Issuer { get; set; }
+        public string Actor { get; set; }
+    }
