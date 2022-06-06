c78f5b19038a9a62c97e79e62f0f98c741784f24
vinhnguyenx1 <Vinhn4@gmail.com>
2022-06-06 16:54:17 +0700

    Create EpochTimeExtensions.cs

---
+++ Infrastructure/Extensions/EpochTimeExtensions.cs
@@ -0,0 +1,16 @@
+public static class EpochTimeExtensions
+    {
+        public static string ToEpoch(this DateTime value)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var elapsedTime = value - epoch;
+            return ((long)elapsedTime.TotalSeconds).ToString(CultureInfo.CurrentCulture);
+        }
+
+        public static long ToEpochNumber(this DateTime value)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var elapsedTime = value - epoch;
+            return (long)elapsedTime.TotalSeconds;
+        }
+    }
