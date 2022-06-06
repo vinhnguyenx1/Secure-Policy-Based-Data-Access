f391ac48b25cb9e75279ec6fd3e0986098322263
vinhnguyenx1 <Vinhn4@gmail.com>
2022-06-06 16:59:02 +0700

    Create PlaybookPolicy.cs

---
+++ Models/Playbook/PlaybookPolicy.cs
@@ -0,0 +1,7 @@
+public class PlaybookPolicy
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Organization { get; set; }
+        public Poort8Policy Policy { get; set; }
+    }
